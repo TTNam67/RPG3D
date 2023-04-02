47d5a8a20d5cc6ac92b7375ea5085213d7e7bff4
ThanhNam <namwizard67@gmail.com>
2023-04-03 00:02:01 +0700

    Demo for the Attack-process

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using RPG.Movement;
+using RPG.Combat;
 using UnityEngine;
 
 //Responsible for doing raycast
@@ -15,6 +17,27 @@ namespace RPG.Control
 
         void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target); //Get our sibling component
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
@@ -23,16 +46,20 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             // "out" giống với "ref" nhưng có khác đôi chút. Tra chatGPT để biết thêm
-            bool hasHit = Physics.Raycast(ray, out hit); //Has hit or not?
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit); //Has hit or not?
 
             if (hasHit == true)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -29,5 +29,3 @@ namespace RPG.Movement
     }
 }
 
-
-
