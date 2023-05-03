dc87905a7ad832782c13a3fb13bc4ad76f7491dd
ThanhNam <namwizard67@gmail.com>
2023-05-03 19:47:03 +0700

    Add: Player cant be controlled during Cut scene

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Control;
+using RPG.Core;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -8,24 +10,33 @@ namespace RPG.Cinematics
     public class CinematicControlRemover : MonoBehaviour
     {
         private PlayableDirector _playableDirector;
+        private GameObject _player;
         private void Start() 
         {
             _playableDirector = GetComponent<PlayableDirector>();
             if (_playableDirector == null)
                 Debug.LogError("CinematicControlRemover.cs: PlayableDirector is not found");
 
+            _player = GameObject.FindWithTag("Player");
+            if (_player == null)
+                Debug.LogError("CinematicController.cs: player is not found");
+
+            // We cant control the player while the cut scene is playing
             _playableDirector.played += DisableControl;
             _playableDirector.stopped += EnableControl;
         }
         
         void DisableControl(PlayableDirector pd)
         {
-            print("DisableControl");
+
+            // When we hit the CutScene trigger, the current action will be disabled
+            _player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            _player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector pd)
         {
-            print("EnableControl");
+            _player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
